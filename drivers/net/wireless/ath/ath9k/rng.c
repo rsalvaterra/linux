f29db5ff7897279ebc8d2403865698cdbfc591fb
Rui Salvaterra <rsalvaterra@gmail.com>
2020-10-28 14:02:43 +0000

    ath9k: implement ADC entropy collection for older generations
    Signed-off-by: Rui Salvaterra <rsalvaterra@gmail.com>

--- drivers/net/wireless/ath/ath9k/rng.c
+++ drivers/net/wireless/ath/ath9k/rng.c
@@ -19,37 +19,25 @@
 
 #include "ath9k.h"
 #include "hw.h"
-#include "ar9003_phy.h"
+
+static inline int ath9k_hw_get_adc_entropy(struct ath_hw *ah,
+	u32 *buf, const u32 buf_size, u32 *rng_last)
+{
+	return ath9k_hw_ops(ah)->get_adc_entropy(ah, buf, buf_size, rng_last);
+}
 
 static int ath9k_rng_data_read(struct ath_softc *sc, u32 *buf, u32 buf_size)
 {
-	int i, j;
-	u32  v1, v2, rng_last = sc->rng_last;
 	struct ath_hw *ah = sc->sc_ah;
+	int bytes_read;
 
 	ath9k_ps_wakeup(sc);
 
-	REG_RMW_FIELD(ah, AR_PHY_TEST(ah), AR_PHY_TEST_BBB_OBS_SEL, 1);
-	REG_CLR_BIT(ah, AR_PHY_TEST(ah), AR_PHY_TEST_RX_OBS_SEL_BIT5);
-	REG_RMW_FIELD(ah, AR_PHY_TEST_CTL_STATUS(ah), AR_PHY_TEST_CTL_RX_OBS_SEL, 0);
-
-	for (i = 0, j = 0; i < buf_size; i++) {
-		v1 = REG_READ(ah, AR_PHY_TST_ADC) & 0xffff;
-		v2 = REG_READ(ah, AR_PHY_TST_ADC) & 0xffff;
-
-		/* wait for data ready */
-		if (v1 && v2 && rng_last != v1 && v1 != v2 && v1 != 0xffff &&
-		    v2 != 0xffff)
-			buf[j++] = (v1 << 16) | v2;
-
-		rng_last = v2;
-	}
+	bytes_read = ath9k_hw_get_adc_entropy(ah, buf, buf_size, &sc->rng_last);
 
 	ath9k_ps_restore(sc);
 
-	sc->rng_last = rng_last;
-
-	return j << 2;
+	return bytes_read;
 }
 
 static u32 ath9k_rng_delay_get(u32 fail_stats)
@@ -95,12 +83,8 @@ static int ath9k_rng_read(struct hwrng *rng, void *buf, size_t max, bool wait)
 void ath9k_rng_start(struct ath_softc *sc)
 {
 	static atomic_t serial = ATOMIC_INIT(0);
-	struct ath_hw *ah = sc->sc_ah;
 
 	if (sc->rng_ops.read)
-		return;
-
-	if (!AR_SREV_9300_20_OR_LATER(ah))
 		return;
 
 	snprintf(sc->rng_name, sizeof(sc->rng_name), "ath9k_%u",
